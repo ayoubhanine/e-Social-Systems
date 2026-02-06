8246dec69dd51db294e23ce440d8e2298eb49e1c
walid-hud <walidthehud@gmail.com>
2026-02-06 15:24:42 +0100

    add some utility functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 /** * 
  @param {string} selector - the css selector of the element to select
+ *@returns {Element} - the selected element or throws an error if the element is not found
  *   
 */
 export const select = (selector)=>{
@@ -20,6 +21,41 @@ export function css(strings , ...values){
     } , "")
 }
 
+
+/**
+ * 
+ * @param {number} ms - the number of milliseconds to sleep
+ * @returns {Promise<void>} - a promise that resolves after the given number of milliseconds
+ */
+// a function to sleep for a given number of milliseconds
 export function sleep(ms){
     return new Promise(resolve => setTimeout(resolve , ms))
 }
+
+
+/**
+ * @param {Function} func - the function to debounce
+ * @param {number} delay - the delay in milliseconds
+ * @return {Function} - the debounced function
+ */
+// debounce function to limit the number of times a function is called
+export function debounce(func , delay){
+    let timeoutId
+    return function(...args){
+        if(timeoutId) clearTimeout(timeoutId)
+        timeoutId = setTimeout(()=>{
+            func.apply(this , args)
+        } , delay)
+    }
+}
+
+/**
+ * 
+ * @returns {string}
+ */
+// generate a random 8 characters id  
+export function generate_id(){
+    return crypto.randomUUID().trim().slice(0 , 8) 
+}
+
+
