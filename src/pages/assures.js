0ebb1db3589ba76a50bd37f1c450eaf70ce9de50
ayoubhanine <ayoubhanine2000@gmail.com>
2026-02-10 17:32:38 +0100

    remplacer button modier avec une icon

--- src/pages/assures.js
+++ src/pages/assures.js
@@ -60,11 +60,11 @@ function template() {
           <select id="employeur" class="epaiceur">
             <option value="">--Choisir--</option>
             ${get_all_employers()
-              .map(
-                (e) =>
-                  `<option value="${e.id}">${e.company_name}</option>`
-              )
-              .join("")}
+      .map(
+        (e) =>
+          `<option value="${e.id}">${e.company_name}</option>`
+      )
+      .join("")}
           </select>
         </label>
 
@@ -155,15 +155,18 @@ function styles() {
     }
 
     .btn-edit-salaire {
-      margin-left: 0.5rem;
-  font-size: 0.75rem;
-  padding: 0.3rem 0.6rem;
-  background: #e0e7ff;
-  color: #1e40af;
+  display: flex;
+  align-items: center;      
+  justify-content: center;  
+  padding: 4px;
+  background: transparent;
   border: none;
-  border-radius: 6px;
   cursor: pointer;
-    }
+}
+
+  svg{
+ 
+  }
   .btn-edit-salaire:hover {
   background: #c7d2fe;
 }
@@ -301,14 +304,22 @@ function script() {
     employees.forEach((emp, index) => {
       const tr = document.createElement("tr");
       tr.dataset.index = index;
-      tr.innerHTML = `
+      tr.innerHTML = html`
         <td>${emp.id}</td>
         <td>${emp.name}</td>
         <td>
           ${emp.salary}
-          <button class="btn-edit-salaire">Modifier</button>
+          <button class="btn-edit-salaire">
+          <svg width="15" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 3H5C4.46957 3 3.96086 3.21071 3.58579 3.58579C3.21071 3.96086 3 4.46957 3 5V19C3 19.5304 3.21071 20.0391 3.58579 20.4142C3.96086 20.7893 4.46957 21 5 21H19C19.5304 21 20.0391 20.7893 20.4142 20.4142C20.7893 20.0391 21 19.5304 21 19V12" 
+stroke="red"
+stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M18.375 2.62498C18.7728 2.22716 19.3124 2.00366 19.875 2.00366C20.4376 2.00366 20.9771 2.22716 21.375 2.62498C21.7728 3.02281 21.9963 3.56237 21.9963 4.12498C21.9963 4.68759 21.7728 5.22716 21.375 5.62498L12.362 14.639C12.1245 14.8762 11.8312 15.0499 11.509 15.144L8.63597 15.984C8.54992 16.0091 8.45871 16.0106 8.37188 15.9883C8.28505 15.9661 8.2058 15.9209 8.14242 15.8575C8.07904 15.7942 8.03386 15.7149 8.01162 15.6281C7.98937 15.5412 7.99087 15.45 8.01597 15.364L8.85597 12.491C8.9505 12.169 9.12451 11.876 9.36197 11.639L18.375 2.62498Z"
+ stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+          </button>
         </td>
-        <td>${[...EMPLOYERS.values()].find(e=>e.get_employee(emp.id))?.company_name}</td>
+        <td>${[...EMPLOYERS.values()].find(e => e.get_employee(emp.id))?.company_name}</td>
         <td><a href="#" class="link-droits">Droits</a></td>
       `;
       tableBody.appendChild(tr);
@@ -331,7 +342,7 @@ function script() {
 
     if (!nomComplet.value || !salaire.value) return;
 
-    
+
     const emp = new Employee(nomComplet.value, Number(salaire.value));
     let employerId = employeurSelect.value;
     emp.months_declared = 8; // exemple
